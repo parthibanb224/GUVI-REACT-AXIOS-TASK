96f3ba8e3b432243fcfcfea4c5e5caebb6fed661
parthibanb224 <parthibanb224@gmail.com>
2023-07-17 11:56:30 +0530

    Axios Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import UserDetails from './pages/UserDetails';
+import Add from './pages/Add';
+import Edit from './pages/Edit';
+import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="container">
-      <UserDetails />
+      <Routes>
+        <Route path='/' Component={UserDetails}></Route>
+        <Route path='/add' Component={Add}></Route>
+        <Route path='/Edit/:id' Component={Edit}></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -1,57 +1,34 @@
-import React, { useEffect, useState } from 'react'
 import UserTable from '../component/UserTable'
 import Table from 'react-bootstrap/Table';
-import Axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import { TiPlus } from "react-icons/ti";
+import { Link } from 'react-router-dom';
+import {useUser} from '../context/user.context';
 
 export default function UserDetails() {
-    const [user, setUser] = useState([]);
-
-    useEffect(() => {
-        Axios.get("/mocks/users.json")
-            .then(response => setUser(response.data))
-            .catch(err => console.log("Axios get Error ::", err))
-    }, [])
-
-    function addNewUser(data) {
-        setUser([...user, { ...data }])
-    }
-
-    function saveData() {
-        Axios.post("https://jsonplaceholder.typicode.com/users",
-            {
-                id: 11,
-                name: "Parthiban",
-                username: "parthi",
-                email: "parthi@gmail.com",
-                phone: 9232432522,
-                website: "www.google.com",
-            })
-            .then(response => addNewUser(response.data))
-            .catch(err => console.log("Axios Post Error ::", err))
-    }
+    const {user,handleDelete} = useUser()
 
     return (
         <div>
+            <h1 className='text-center mt-3'>REACT AXIOS TASK</h1>
             <div className='mt-5 d-flex justify-content-between'>
                 <h3>User Details</h3>
-                <Button onClick={saveData} className='button' size='sm' variant="primary"><TiPlus /> Add New</Button>{' '}
+                <Link to='/add'><Button className='button' size='sm' variant="primary"><TiPlus /> Add New</Button>{' '}</Link>
             </div>
             <Table className='mt-3' striped bordered hover size='xl'>
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>FIRST NAME</th>
                         <th>USERNAME</th>
                         <th>EMAIL</th>
                         <th>PHONE</th>
                         <th>WEBSITE</th>
+                        <th>ACTIONS</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        user.map((item, i) => <UserTable key={i} data={item} />)
+                        user.map((item, i) => <UserTable key={i} data={item} handleDelete={handleDelete} />)
                     }
                 </tbody>
             </Table>
